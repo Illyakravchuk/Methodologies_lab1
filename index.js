95322740167ad6e139cd72fed8f4b45948f43b35
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-03-06 16:06:51 +0200

    added function to work with interactive input mode
    In interactive mode, the application starts when no arguments have been passed to it. In this mode, the application asks to enter 3 coefficients in turn: a, b and c, displays the resulting equation on the screen and the found roots

--- index.js
+++ index.js
@@ -15,3 +15,45 @@ function solve(a, b, c) {
         return console.log(`x1 = ${x1.toFixed(1)}\nx2 = ${x2.toFixed(1)}`);
     }
 }
+
+function interactiveMode() {
+    const abc = ["a= ", "b= ", "c= "];
+    const params = [];
+    let i = 0;
+    ask(i);
+    process.stdin.on("data", (data) => {
+        if (params.length < abc.length) {
+            if (!isNum(data)) {
+                console.error(`
+                    Error. Expected a valid real number, got ${data.toString().trim()} instead
+            `);
+                ask(i);
+            } else if (data.toString().trim() == 0 && params.length == 0) {
+                console.error(`Error. a cannot be 0`);
+                ask(i);
+            } else {
+                params.push(+data.toString().trim());
+                i++;
+                if (params.length < abc.length) {
+                    ask(i);
+                } else {
+                    solve(...params);
+                    process.exit();
+                }
+            }
+        }
+    });
+
+    function ask(i) {
+        process.stdout.write(abc[i]);
+    }
+}
+
+function isNum(value) {
+    const numeric = parseFloat(value.toString().trim());
+    if (isNaN(numeric)) {
+        return false;
+    }
+    return true;
+}
+interactiveMode()
